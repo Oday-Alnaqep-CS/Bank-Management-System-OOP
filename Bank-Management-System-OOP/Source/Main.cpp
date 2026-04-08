922e0512db5eb34f5ca4853309b9721e55686a9d
Dev. Oday Al-Naqep <alnaqepoday105@gmail.com>
2026-04-08 23:17:45 +0300

    Update Main.cpp

--- Bank-Management-System-OOP/Source/Main.cpp
+++ Bank-Management-System-OOP/Source/Main.cpp
@@ -1,23 +1,23 @@
-//#include <iostream>    
-//#include "ClsLoginScreen.h"
-//
-//
-//using namespace std;
-//
-//int main()
-//
-//{ 
-//    while(true)
-//    {
-//        if (!ClsLoginScreen::ShowLoginScreen())
-//        {
-//            break;
-//        }
-//    }
-//
-//    
-//    return 0;
-//}
+#include <iostream>    
+#include "ClsLoginScreen.h"
+
+
+using namespace std;
+
+int main()
+
+{ 
+    while(true)
+    {
+        if (!ClsLoginScreen::ShowLoginScreen())
+        {
+            break;
+        }
+    }
+
+    
+    return 0;
+}
 
 
 
